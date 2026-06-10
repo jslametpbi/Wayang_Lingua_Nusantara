9f356f5058f91629cf8ef8b193490626d3e48c31
jslametpbi <jslamet.pbi@gmail.com>
2026-06-11 01:16:15 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,3 +0,0 @@
-// V6 intentionally avoids caching so GitHub Pages does not keep an older app version.
-self.addEventListener('install', event => self.skipWaiting());
-self.addEventListener('activate', event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))));
